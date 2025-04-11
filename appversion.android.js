759f1932fa7ca610b6ab932fa91f5c5bd68a0468
Kang Cahya <cahyadyazin@yahoo.com>
2025-04-11 18:53:54 +0700

    Fixed error to run on Nativescript 8.8 or later

--- appversion.android.js
+++ appversion.android.js
@@ -1,67 +1,78 @@
-var application = require("@nativescript/core/application");
+import { Application } from "@nativescript/core/application";
 
-exports.getAppId = function() {
-  return new Promise(function(resolve, reject) {
+export function getAppId() {
+  return new Promise(function (resolve, reject) {
     function _resolve() {
-      var context = application.android.context;
+      const context = Application.android.context; // Gunakan context
       resolve(context.getPackageName());
     }
     try {
-      if (application.android.context) {
+      if (Application.android.context) {
         _resolve();
       } else {
         // if this is called before application.start() wait for the event to fire
-        application.on(application.launchEvent, _resolve);
+        Application.on(Application.launchEvent, _resolve);
       }
     } catch (ex) {
       console.log("Error in appversion.getAppId: " + ex);
       reject(ex);
     }
   });
-};
+}
 
-exports.getAppIdSync = function() {
-  return application.android.context.getPackageName();
-};
+export function getAppIdSync() {
+  return Application.android.context.getPackageName(); // Gunakan context
+}
 
-exports.getVersionName = function() {
-  return new Promise(function(resolve, reject) {
+export function getVersionName() {
+  return new Promise(function (resolve, reject) {
     function _resolve() {
-      var packageManager = application.android.context.getPackageManager();
-      resolve(packageManager.getPackageInfo(application.android.context.getPackageName(), 0).versionName);
+      const context = Application.android.context; // Gunakan context
+      const packageManager = context.getPackageManager();
+      resolve(
+        packageManager.getPackageInfo(context.getPackageName(), 0).versionName
+      );
     }
     try {
-      if (application.android.context) {
+      if (Application.android.context) {
         _resolve();
       } else {
         // if this is called before application.start() wait for the event to fire
-        application.on(application.launchEvent, _resolve);
+        Application.on(Application.launchEvent, _resolve);
       }
     } catch (ex) {
       console.log("Error in appversion.getVersionName: " + ex);
       reject(ex);
     }
   });
-};
+}
 
-exports.getVersionNameSync = function() {
-  var packageManager = application.android.context.getPackageManager();
-  return packageManager.getPackageInfo(application.android.context.getPackageName(), 0).versionName;
-};
+export function getVersionNameSync() {
+  const context = Application.android.context; // Gunakan context
+  const packageManager = context.getPackageManager();
+  return packageManager.getPackageInfo(context.getPackageName(), 0).versionName;
+}
 
-exports.getVersionCode = function() {
-  return new Promise(function(resolve, reject) {
+export function getVersionCode() {
+  return new Promise(function (resolve, reject) {
     try {
-      var packageManager = application.android.context.getPackageManager();
-      resolve("" + packageManager.getPackageInfo(application.android.context.getPackageName(), 0).versionCode);
+      const context = Application.android.context; // Gunakan context
+      const packageManager = context.getPackageManager();
+      resolve(
+        "" +
+          packageManager.getPackageInfo(context.getPackageName(), 0).versionCode
+      );
     } catch (ex) {
       console.log("Error in appversion.getVersionCode: " + ex);
       reject(ex);
     }
   });
-};
+}
 
-exports.getVersionCodeSync = function() {
-  var packageManager = application.android.context.getPackageManager();
-  return "" + packageManager.getPackageInfo(application.android.context.getPackageName(), 0).versionCode;
-};
+export function getVersionCodeSync() {
+  const context = Application.android.context; // Gunakan context
+  const packageManager = context.getPackageManager();
+  return (
+    "" + packageManager.getPackageInfo(context.getPackageName(), 0).versionCode
+  );
+}
